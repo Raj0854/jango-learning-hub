3caa7a2822918a937fac2e09d0b10b7682f12d31
Raj gupta <rajgupta.02.0854@gmail.com>
2026-08-08 16:38:07 +0530

    Update views.py

--- learning_hub/webpages/views.py
+++ learning_hub/webpages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import UserProfile
 # Create your views here.
 def home(request):
     return render(request,"home.html")
@@ -11,3 +12,6 @@ def about(request):
     return render(request,"about.html")
 def contact(request):
     return render(request,"contact.html")
+def profiles(request):
+    profiles =UserProfile.objects.all()
+    return render(request,"profile.html",{'profiles' :profiles})

982f32412a697d39004cb7ba4dff9b227ca6d211
Raj gupta <rajgupta.02.0854@gmail.com>
2026-08-08 16:37:38 +0530

    Update models.py

--- learning_hub/webpages/models.py
+++ learning_hub/webpages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    firstName=models.CharField(max_length=100)
+    lastName=models.CharField(max_length=100)
+    # Date_of_joining=models.CharField(max_length=100)
+    Email=models.EmailField(unique=True)
+    courses=models.CharField(default="python")
+    

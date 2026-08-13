69f4066f0e66deba9413a4e194678afd5ec90cd3
Raj gupta <rajgupta.02.0854@gmail.com>
2026-08-13 17:07:32 +0530

    updated files

--- learning_hub/webpages/models.py
+++ learning_hub/webpages/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class UserProfile(models.Model):
     firstName=models.CharField(max_length=100)
     lastName=models.CharField(max_length=100)
-    # Date_of_joining=models.CharField(max_length=100)
+    contactNumber=models.CharField(max_length=15,null=True)
+    Date_of_Enquiry=models.DateField(default=timezone.localdate)
     Email=models.EmailField(unique=True)
-    courses=models.CharField(default="python")
+    courses=models.CharField(null=True)
     
